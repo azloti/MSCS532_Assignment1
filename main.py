bc4152e9dc77dbe1301ab97119be210b3fac1d7a
adrianUC <nichita@me.com>
2024-08-26 19:25:27 -0400

    Add monotonically decreasing insertion sort

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-def ins_sort(arr: list) -> None:
-    raise NotImplementedError("Insertion sort not yet implemented")
+import random
 
-# Example usage
-source = [5,4,3,2,1]
-insertion_sort(source)
-print(source)
+def ins_sort(arr: list) -> None:
+    # Loop through the array starting from the second element
+    for currentIndex in range(1, len(arr)):
+        currentValue = arr[currentIndex]
+        previousIndex = currentIndex - 1 # Start from the previous element
+        while previousIndex >= 0 and currentValue > arr[previousIndex]: # While the previous element is greater than the current value
+            # Shift the previous element to the right
+            arr[previousIndex + 1] = arr[previousIndex]
+            previousIndex -= 1
+        arr[previousIndex + 1] = currentValue # Insert the currentValue in the correct position
+
+# Source array
+arr = [4, 5, 1, 2, 3]
+
+# Sort the array
+ins_sort(arr)
+
+# Print the sorted array
+print(arr)
